e9afc9be23be802f13a18c0c10a2540dfe60fa3f
carminenicotera <carmine.91.n@hotmail.it>
2026-02-03 15:44:32 +0100

    add condition multiple of 3

--- js/main.js
+++ js/main.js
@@ -4,5 +4,9 @@ const multiCinque = "Buzz";
 
 //Uso un ciclo for per scansionare i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-  
+
+  //Eseguo i controlli condizionali per verificare se il numero è solo multiplo di 3
+  if (i % 3 === 0 && i % 5 !== 0) {
+    console.log(multiTre);
+  }
 }
